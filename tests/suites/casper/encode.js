7b1882ee7b380aff071d2117f6b096fee5afb2ca
Marc Epard <mepard@me.com>
2015-12-31 16:11:46 -0600

    base64encode fails on PhantomJS 2.1
    Skip it for now.

--- tests/suites/casper/encode.js
+++ tests/suites/casper/encode.js
@@ -4,6 +4,15 @@ var fs = require('fs');
 casper.test.begin('base64encode() and download() tests', 2, function(test) {
     // FIXME: https://github.com/ariya/phantomjs/pull/364 has been merged, update scheme
     casper.start('file://' + phantom.casperPath + '/tests/site/index.html', function() {
+        if (test.skipIfEngine(2, {
+            name: 'phantomjs',
+            version: {
+                min: '2.1.0',
+                max: '2.1.0'
+            },
+            message: 'phantomjs 2.1 fails base64encode'
+        })) return;
+        
         var imageUrl = 'file://' + phantom.casperPath + '/tests/site/images/phantom.png',
             image = this.base64encode(imageUrl);
         test.assertEquals(image.length, 6160, 'Casper.base64encode() can retrieve base64 contents');
